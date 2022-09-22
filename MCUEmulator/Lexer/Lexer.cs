ce1b0d1cb9a8022b7fb98f80aac0fd5f3e846c90
Igor <vorobiov.777@yandex.ru>
2022-09-22 14:53:36 +0300

    Added token association

--- MCUEmulator/Lexer/Lexer.cs
+++ MCUEmulator/Lexer/Lexer.cs
@@ -1,5 +1,28 @@
 ﻿namespace MCUEmulator.Lexer;
 
+/*
+ * Tokens:
+ *   Commands:
+ *     - MOV
+ *     - SUB
+ *     ...
+ *   OPERAND:
+ *     - r.. - регистр
+ *     - 0x.. - ячейка памяти
+ *     - b... \ 
+ *     - d... -  константы
+ *     - h... /
+ *     ...
+ *
+ *    NEWLINE:
+ *     - \n
+ *
+ *    EOF:
+ *     - \0
+ *
+ *    SEPARATOR:
+ *     - ,
+ */
 public class Lexer
 {
     
