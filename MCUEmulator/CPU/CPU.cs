bc25de77b90e770b989fca212451c919a50528a4
Igor <vorobiov.777@yandex.ru>
2022-09-28 16:25:10 +0300

    Added extend method for BitArray to resize it. Added function to convert int value to BitArray Added function SetRegisterValue
    No one of this function was accuratly covered with tests =)

--- MCUEmulator/CPU/CPU.cs
+++ MCUEmulator/CPU/CPU.cs
@@ -1,6 +1,85 @@
-﻿namespace MCUEmulator;
+﻿using System.Collections;
 
-public class CPU
+namespace MCUEmulator;
+
+
+public class Cpu
 {
+    private const int InstructionMemorySize = 34;
     
+    private const int RegisterSize = 8;
+
+    private Random _rnd = new Random((int)DateTime.Now.Ticks);
+    // Instruction Memory
+    private List<Register> _instractionsMemory = new(InstructionMemorySize);
+    // Data Memory
+    private List<Register> _dataMemory = new(InstructionMemorySize);
+
+    private Dictionary<string, int> _ronNameAssociations = new Dictionary<string, int>
+    {
+        {"rax", 0}, // 0000 0000 | 0x00 
+        {"rbx", 1}, // 0000 0001 | 0x01
+        {"pcx", 2}, // 0000 0010 | 0x02
+        {"rdx", 3}, // 0000 0011 | 0x03
+        {"trx", 4}, // 0000 0100 | 0x04
+        {"flx", 5}, // 0000 0101 | 0x05
+        {"fmx", 6}, // 0000 0110 | 0x06
+    };
+
+    public Cpu()
+    {
+        InitializeMemories();
+        // PrintMemory(_instractionsMemory);
+        // PrintMemory(_dataMemory);
+        PrintMemories((new List<IList<Register>>{ _instractionsMemory, _dataMemory }));
+    }
+
+    public void PrintMemory(IList<Register> memory)
+    {
+        Console.ForegroundColor = (ConsoleColor)_rnd.Next(0, 15);
+        foreach (var reg in memory)
+        {
+            Console.WriteLine(reg.ToString());
+        }
+        Console.ResetColor();
+    }
+
+    public void PrintMemories(IList<IList<Register>> memList)
+    {
+        int[] consoleColors = new int [memList.Count];
+        for (int i = 0; i < consoleColors.Length; i++)
+        {
+            consoleColors[i] = _rnd.Next(0, 15);
+        }
+        for (int i = 0; i < memList[0].Count; i++)
+        {
+            for (int j = 0; j < memList.Count; j++)
+            {
+                Console.ForegroundColor = (ConsoleColor)consoleColors[j];
+                Console.Write($"{memList[j][i].ToString()}\t");
+            }
+
+            Console.WriteLine();
+        }
+        Console.ResetColor();
+    }
+    
+    
+
+    private void InitializeMemories()
+    {
+        // Initialize Memories with registers
+        for (int i = 0; i < InstructionMemorySize; i++)
+        {
+            _instractionsMemory.Add( new Register(RegisterSize));
+            _instractionsMemory[i].SetRegisterValue(16);
+            _dataMemory.Add(new Register(RegisterSize));
+        }
+    }
+
+    public void SetRegisterValue(string registerName, BitArray value)
+    {
+        if (value.Count > RegisterSize)
+            throw new ArgumentException($"Too many bits({value.Count}) in argument");
+    }
 }
--- MCUEmulator/CPU/Register.cs
+++ MCUEmulator/CPU/Register.cs
@@ -5,27 +5,27 @@ namespace MCUEmulator;
 
 public class Register
 {
-    private BitArray bits;
+    private BitArray _bits;
     public Register(int size = 8)
     {
-        bits = new(size);
+        _bits = new(size);
     }
 
-    public bool getBit(int index)
+    public bool GetBit(int index)
     {
-        return bits[index];
+        return _bits[index];
     }
 
-    public void setBit(int index, bool value)
+    public void SetBit(int index, bool value)
     {
-        bits[index] = value;
+        _bits[index] = value;
     }
 
     public override string ToString()
     {
         var builder = new StringBuilder();
         builder.Append($"[");
-        foreach (bool bit in bits)
+        foreach (bool bit in _bits)
         {
             builder.Append($" {(bit ? 1 : 0)} ");
         }
@@ -33,9 +33,56 @@ public class Register
         builder.Append("]");
         return builder.ToString();
     }
+
+    public void SetRegisterValue(BitArray value)
+    {
+        _bits = value;
+    }
+
+    public void SetRegisterValue(int value)
+    {
+        if (value > Math.Pow(2, _bits.Count))
+            throw new ArgumentException($"To big value for {_bits.Count} bits");
+        // _bits = new BitArray(value);
+        var tmp = fromDecToBin(value);
+        _bits = tmp.resizeUpTo(_bits.Count);
+
+    }
+
+    private BitArray fromDecToBin(int value)
+    {
+        List<bool> result = new List<bool>();
+        while (value > 0)
+        {
+            int m = value % 2;
+            value = value / 2;
+            result.Add(m == 1);
+        }
+        result.Reverse();
+        return new BitArray(result.ToArray());
+    }
+    
     
     /*
      * TODO
      *  1. Сдвиг цикличный, обычный
      */
+}
+
+public static class BitArrayExtension
+{
+    // Have 0010 need extend upto 8 bits
+    public static BitArray resizeUpTo(this BitArray array, int to)
+    {
+        var oldArraySize = array.Count;
+        if (oldArraySize > to)
+            throw new ArgumentException($"You can't extend array with size {oldArraySize}, to lower count {to} bits");
+        var newArray = new BitArray(to, false);
+        for (int i = 0; i < oldArraySize; i++)
+        {
+            newArray[^(i + 1)] = array[^(i + 1)];
+        }
+
+        return newArray;
+    }
 }
