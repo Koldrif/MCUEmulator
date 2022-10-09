ac68146dbcd0e31b0a5aa4ab9f8b4e0efe82c23d
dushanova45 <dushanova45@gmail.com>
2022-10-09 22:54:44 +0300

    Add Lexer, but need to finish regular expressions Need to supplement them with new commands and so on

--- MCUEmulator/Program.cs
+++ MCUEmulator/Program.cs
@@ -2,10 +2,11 @@
 
 using MCUEmulator;
 using MCUEmulator.CPU;
+using MCUEmulator.Lexer;
 
 // var cpu = new Cpu();
 
-var testReg = new Register();
+/*var testReg = new Register();
 
 testReg.SetRegisterValue(0xf1);
 
@@ -17,6 +18,48 @@ Console.WriteLine(testReg.ToString());
 
 testReg.Shift(Register.ShiftDirection.Right);
 
-Console.WriteLine(testReg.ToString());
+Console.WriteLine(testReg.ToString());*/
 
-          
+
+
+
+
+    StreamReader stream = OpenFileVisualInterface(args);
+    List<Token> tokens;
+    try
+    {
+        // Получаем токены.
+        tokens = new LexerLang().SearchTokens(stream);
+    }
+    catch(LexerException e)
+    {
+        // Ошибка.
+        Console.WriteLine(e + "\n" + e.StackTrace);
+        return 5;
+    }
+    stream.Close();
+    foreach (Token token in tokens)
+        // Печатаем токины.
+        Console.WriteLine(token);
+    Console.Write("Press eny key...");
+    Console.ReadLine();
+
+
+static StreamReader OpenFileVisualInterface(string[] args)
+{
+    FileInfo input;
+    if (args.Length != 1)
+    { // Если файл из аргументов программы не взят
+        Console.Write("Name file: ");
+        input = new FileInfo(Console.ReadLine());
+    }
+    else
+    { // Если есть аргументы, то берём из аргументов.
+        input = new FileInfo(args[0]);
+    }
+    return input.OpenText();
+}
+
+    return 0;
+
+ 
